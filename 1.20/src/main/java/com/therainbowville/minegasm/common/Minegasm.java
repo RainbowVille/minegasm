d7cd4dbdc69abcb1beba97aef62ba5f7f4f2c7ba
blackspherefollower <darktech@black-sphere.co.uk>
2023-06-27 00:43:29 -0700

    wip: Break out the 1.19 and 1.20 builds for API changes
    There's plenty of room here for common shared code

---
+++ 1.20/src/main/java/com/therainbowville/minegasm/common/Minegasm.java
@@ -0,0 +1,25 @@
+package com.therainbowville.minegasm.common;
+
+import com.therainbowville.minegasm.config.ConfigHolder;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Minegasm.MOD_ID)
+public class Minegasm
+{
+    public static final String MOD_ID = "minegasm";
+    public static final String NAME = "Minegasm";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Minegasm() {
+        ModLoadingContext context = ModLoadingContext.get();
+        context.registerConfig(ModConfig.Type.CLIENT, ConfigHolder.CLIENT_SPEC);
+        context.registerConfig(ModConfig.Type.SERVER, ConfigHolder.SERVER_SPEC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
