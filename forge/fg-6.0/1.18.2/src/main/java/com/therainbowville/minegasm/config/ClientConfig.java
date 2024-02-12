af65307b2a72fe99f39a33ca09f5fb60e18bf0e4
Sour3 <31393967+Sour-o7@users.noreply.github.com>
2024-02-11 22:37:03 -0800

    Update ClientConfig.java
    Added more Intensity Parameters
    Added Reset functions

--- forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/config/ClientConfig.java
+++ forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/config/ClientConfig.java
@@ -1,9 +1,11 @@
 package com.therainbowville.minegasm.config;
 
 import com.therainbowville.minegasm.common.Minegasm;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.mclanguageprovider.MinecraftModLanguageProvider;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 
 import java.util.Objects;
 
@@ -16,17 +18,34 @@ public final class ClientConfig {
     final ForgeConfigSpec.IntValue attackIntensity;
     final ForgeConfigSpec.IntValue hurtIntensity;
     final ForgeConfigSpec.IntValue mineIntensity;
+    final ForgeConfigSpec.IntValue placeIntensity;
     final ForgeConfigSpec.IntValue xpChangeIntensity;
+    final ForgeConfigSpec.IntValue fishingIntensity;
     final ForgeConfigSpec.IntValue harvestIntensity;
     final ForgeConfigSpec.IntValue vitalityIntensity;
     final ForgeConfigSpec.IntValue advancementIntensity;
+    
+    static final String DEFAULT_SERVER_URL = "ws://localhost:12345/buttplug";
+    static final boolean DEFAULT_VIBRATE = true;
+    static final GameplayMode DEFAULT_MODE = GameplayMode.NORMAL;
+    static final boolean DEFAULT_STEALTH = false;
+    
+    static final int DEFAULT_ATTACK_INTENSITY = 60;
+    static final int DEFAULT_HURT_INTENSITY = 0;
+    static final int DEFAULT_MINE_INTENSITY = 80;
+    static final int DEFAULT_PLACE_INTENSITY = 20;
+    static final int DEFAULT_XP_CHANGE_INTENSITY = 100;
+    static final int DEFAULT_FISHING_INTENSITY = 50;
+    static final int DEFAULT_HARVEST_INTENSITY = 0;
+    static final int DEFAULT_VITALITY_INTENSITY = 0;
+    static final int DEFAULT_ADVANCEMENT_INTENSITY = 100;
 
     ClientConfig(final ForgeConfigSpec.Builder builder) {
         builder.push("buttplug");
 
         serverUrl = builder
                 .translation(Minegasm.MOD_ID + ".config.serverUrl")
-                .define("serverUrl", "ws://localhost:12345/buttplug");
+                .define("serverUrl", DEFAULT_SERVER_URL);
 
         builder.pop();
 
@@ -34,54 +53,83 @@ public final class ClientConfig {
 
         vibrate = builder
                 .translation(Minegasm.MOD_ID + ".config.vibrate")
-                .define("vibrate", true);
+                .define("vibrate", DEFAULT_VIBRATE);
         mode = builder
                 .translation(Minegasm.MOD_ID + ".config.mode")
-                .defineEnum("mode", GameplayMode.NORMAL);
+                .defineEnum("mode", DEFAULT_MODE);
         stealth = builder
                 .translation(Minegasm.MOD_ID + ".config.stealth")
-                .define("stealth", false);
+                .define("stealth", DEFAULT_STEALTH);
 
         builder.push("intensity");
 
         attackIntensity = builder
                 .comment("Vibration intensity when attacking on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.attack")
-                .defineInRange("attackIntensity", 60, 0, 100);
+                .defineInRange("attackIntensity", DEFAULT_ATTACK_INTENSITY, 0, 100);
 
         hurtIntensity = builder
                 .comment("Vibration intensity when hurting on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.hurt")
-                .defineInRange("hurtIntensity", 0, 0, 100);
+                .defineInRange("hurtIntensity", DEFAULT_HURT_INTENSITY, 0, 100);
 
         mineIntensity = builder
                 .comment("Vibration intensity when mining on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.mine")
-                .defineInRange("mineIntensity", 80, 0, 100);
+                .defineInRange("mineIntensity", DEFAULT_MINE_INTENSITY, 0, 100);
+                
+        placeIntensity = builder
+                .comment("Vibration intensity when placing blocks on custom mode")
+                .translation(Minegasm.MOD_ID + ".config.intensity.place")
+                .defineInRange("placeIntensity", DEFAULT_PLACE_INTENSITY, 0, 100);
 
         xpChangeIntensity = builder
                 .comment("Vibration intensity when gaining XP on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.xp_change")
-                .defineInRange("xpChangeIntensity", 100, 0, 100);
+                .defineInRange("xpChangeIntensity", DEFAULT_XP_CHANGE_INTENSITY, 0, 100);
+                
+        fishingIntensity = builder
+                .comment("Vibration intensity when fishing on custom mode")
+                .translation(Minegasm.MOD_ID + ".config.intensity.fishing")
+                .defineInRange("fishingIntensity", DEFAULT_FISHING_INTENSITY, 0, 100);
 
         harvestIntensity = builder
                 .comment("Vibration intensity when harvesting on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.harvest")
-                .defineInRange("harvestIntensity", 0, 0, 100);
+                .defineInRange("harvestIntensity", DEFAULT_HARVEST_INTENSITY, 0, 100);
 
         vitalityIntensity = builder
                 .comment("Vibration intensity on high level of player's vitality on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.vitality")
-                .defineInRange("vitalityIntensity", 0, 0, 100);
+                .defineInRange("vitalityIntensity", DEFAULT_VITALITY_INTENSITY, 0, 100);
                 
         advancementIntensity = builder
                 .comment("Vibration intensity on achieving advancement on custom mode")
                 .translation(Minegasm.MOD_ID + ".config.intensity.advancement")
-                .defineInRange("advancementIntensity", 100, 0, 100);
+                .defineInRange("advancementIntensity", DEFAULT_ADVANCEMENT_INTENSITY, 0, 100);
 
         builder.pop();
         builder.pop();
     }
+    
+    public void resetConfigUrl()
+    {
+        ConfigHolder.CLIENT.serverUrl.set(DEFAULT_SERVER_URL);
+    }
+    
+    public void resetConfigCustom()
+    {
+        ConfigHolder.CLIENT.attackIntensity.set(DEFAULT_ATTACK_INTENSITY);
+        ConfigHolder.CLIENT.hurtIntensity.set(DEFAULT_HURT_INTENSITY);
+        ConfigHolder.CLIENT.mineIntensity.set(DEFAULT_MINE_INTENSITY);
+        ConfigHolder.CLIENT.placeIntensity.set(DEFAULT_PLACE_INTENSITY);
+        ConfigHolder.CLIENT.xpChangeIntensity.set(DEFAULT_XP_CHANGE_INTENSITY);
+        ConfigHolder.CLIENT.fishingIntensity.set(DEFAULT_FISHING_INTENSITY);
+        ConfigHolder.CLIENT.harvestIntensity.set(DEFAULT_HARVEST_INTENSITY);
+        ConfigHolder.CLIENT.vitalityIntensity.set(DEFAULT_VITALITY_INTENSITY);
+        ConfigHolder.CLIENT.advancementIntensity.set(DEFAULT_ADVANCEMENT_INTENSITY);
+    }
+    
     public enum GameplayMode {
         NORMAL("gui." + Minegasm.MOD_ID + ".config.mode.normal"),
         MASOCHIST("gui." + Minegasm.MOD_ID + ".config.mode.masochist"),
@@ -99,4 +147,6 @@ public final class ClientConfig {
             return this.translateKey;
         }
     }
+    
+    
 }
