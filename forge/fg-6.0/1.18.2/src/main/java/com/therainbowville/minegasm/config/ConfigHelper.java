bc2bde207e1976640b00d7d17b8c209826645ed8
Sour3 <31393967+Sour-o7@users.noreply.github.com>
2024-02-11 22:37:54 -0800

    Update ConfigHelper.java
    Added SaveClient Function
    Added Intensity Parameters
    Added ConfigFactory Function

--- forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/config/ConfigHelper.java
+++ forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/config/ConfigHelper.java
@@ -1,6 +1,13 @@
 package com.therainbowville.minegasm.config;
 
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class ConfigHelper {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void bakeClient() {
         MinegasmConfig.serverUrl = ConfigHolder.CLIENT.serverUrl.get();
@@ -10,13 +17,40 @@ public final class ConfigHelper {
         MinegasmConfig.attackIntensity = ConfigHolder.CLIENT.attackIntensity.get();
         MinegasmConfig.hurtIntensity = ConfigHolder.CLIENT.hurtIntensity.get();
         MinegasmConfig.mineIntensity = ConfigHolder.CLIENT.mineIntensity.get();
+        MinegasmConfig.placeIntensity = ConfigHolder.CLIENT.placeIntensity.get();
         MinegasmConfig.xpChangeIntensity = ConfigHolder.CLIENT.xpChangeIntensity.get();
+        MinegasmConfig.fishingIntensity = ConfigHolder.CLIENT.fishingIntensity.get();
         MinegasmConfig.harvestIntensity = ConfigHolder.CLIENT.harvestIntensity.get();
         MinegasmConfig.vitalityIntensity = ConfigHolder.CLIENT.vitalityIntensity.get();
         MinegasmConfig.advancementIntensity = ConfigHolder.CLIENT.advancementIntensity.get();
     }
 
     public static void bakeServer() {
+    }
+    
+    public static void saveClient()
+    {
+        
+        MinegasmConfigBuffer buffer = new MinegasmConfigBuffer();
+        
+        ConfigHolder.CLIENT.serverUrl.set(buffer.serverUrl);
+        ConfigHolder.CLIENT.vibrate.set(buffer.vibrate);
+        ConfigHolder.CLIENT.mode.set(buffer.mode);
+        ConfigHolder.CLIENT.stealth.set(buffer.stealth);
+        ConfigHolder.CLIENT.attackIntensity.set(buffer.attackIntensity);
+        ConfigHolder.CLIENT.hurtIntensity.set(buffer.hurtIntensity);
+        ConfigHolder.CLIENT.mineIntensity.set(buffer.mineIntensity);
+        ConfigHolder.CLIENT.placeIntensity.set(buffer.placeIntensity);
+        ConfigHolder.CLIENT.xpChangeIntensity.set(buffer.xpChangeIntensity);
+        ConfigHolder.CLIENT.fishingIntensity.set(buffer.fishingIntensity);
+        ConfigHolder.CLIENT.harvestIntensity.set(buffer.harvestIntensity);
+        ConfigHolder.CLIENT.vitalityIntensity.set(buffer.vitalityIntensity);
+        ConfigHolder.CLIENT.advancementIntensity.set(buffer.advancementIntensity);
+       
+    }
+    
+    public static ConfigGuiHandler.ConfigGuiFactory createConfigGuiFactory() {
+        return new ConfigGuiHandler.ConfigGuiFactory((minecraft, screen) -> new ConfigScreen(screen));
     }
 
 }
