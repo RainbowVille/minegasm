894b2fba7e1e8627aa40685d99b4bcbc580c296b
Sour3 <turtlewong007@gmail.com>
2024-03-03 21:14:47 -0800

    Added Support for 1.20.4

---
+++ forge/fg-6.0/1.20.4/src/main/java/com/therainbowville/minegasm/common/VibrationStateAdvancement.java
@@ -0,0 +1,47 @@
+package com.therainbowville.minegasm.common;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementType;
+import net.minecraftforge.event.entity.player.AdvancementEvent;
+
+import com.therainbowville.minegasm.config.MinegasmConfig;
+
+public class VibrationStateAdvancement extends AbstractVibrationState
+{
+    public VibrationStateAdvancement()
+    {
+        super(0);
+    }
+    
+    public void onAdvancement(AdvancementEvent event)
+    {
+        if (getIntensity("advancement") == 0) return;
+    try {
+        LOGGER.info("Advancement Event: " + event);
+        Advancement advancement = event.getAdvancement().value();
+        if (advancement == null) return;
+        AdvancementType type = advancement.display().get().getType();
+        int duration = switch (type) {
+            case TASK -> 5;
+            case GOAL -> 7;
+            case CHALLENGE -> 10;
+        };
+
+        vibrationCountdown = duration * MinegasmConfig.ticksPerSecond;
+        vibrationFeedbackCountdown = 1.5f * MinegasmConfig.ticksPerSecond;
+    } catch (Throwable e) {
+            LOGGER.throwing(e);
+    }
+    }
+    
+    public int getIntensity()
+    {
+        if (accumulationEnabled())
+            return Math.toIntExact(Math.round(intensity));
+        else if (vibrationFeedbackCountdown > 0)
+            return Math.min(100, getIntensity("advancement") + 20);
+        else if (vibrationCountdown > 0)
+            return getIntensity("advancement");
+        else return 0;
+    }
+}
--- forge/fg-6.0/1.20.4/src/main/java/com/therainbowville/minegasm/config/ConfigHelper.java
+++ forge/fg-6.0/1.20.4/src/main/java/com/therainbowville/minegasm/config/ConfigHelper.java
@@ -1,20 +1,79 @@
 package com.therainbowville.minegasm.config;
 
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class ConfigHelper {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void bakeClient() {
         MinegasmConfig.serverUrl = ConfigHolder.CLIENT.serverUrl.get();
         MinegasmConfig.vibrate = ConfigHolder.CLIENT.vibrate.get();
         MinegasmConfig.mode = ConfigHolder.CLIENT.mode.get();
+        MinegasmConfig.stealth = ConfigHolder.CLIENT.stealth.get();
+        MinegasmConfig.tickFrequency = ConfigHolder.CLIENT.tickFrequency.get().getInt();
+        MinegasmConfig.ticksPerSecond = Math.max(0, 20f / MinegasmConfig.tickFrequency);
+        
         MinegasmConfig.attackIntensity = ConfigHolder.CLIENT.attackIntensity.get();
         MinegasmConfig.hurtIntensity = ConfigHolder.CLIENT.hurtIntensity.get();
         MinegasmConfig.mineIntensity = ConfigHolder.CLIENT.mineIntensity.get();
+        MinegasmConfig.placeIntensity = ConfigHolder.CLIENT.placeIntensity.get();
         MinegasmConfig.xpChangeIntensity = ConfigHolder.CLIENT.xpChangeIntensity.get();
+        MinegasmConfig.fishingIntensity = ConfigHolder.CLIENT.fishingIntensity.get();
         MinegasmConfig.harvestIntensity = ConfigHolder.CLIENT.harvestIntensity.get();
         MinegasmConfig.vitalityIntensity = ConfigHolder.CLIENT.vitalityIntensity.get();
+        MinegasmConfig.advancementIntensity = ConfigHolder.CLIENT.advancementIntensity.get();
     }
 
     public static void bakeServer() {
+    }
+    
+    public static void saveClient()
+    {
+        try{
+        
+        MinegasmConfigBuffer buffer = new MinegasmConfigBuffer();
+        
+//        Field[] fields = MinegasmConfigBuffer.class.getFields();
+
+//        for (Field field : fields)
+//        {
+//            Field configField = ConfigHolder.CLIENT.getClass().getDeclaredField(field.getName()); // Get Corisponding Field in CLIENT
+//            Class configFieldClass = configField.getType(); // Get the configField Class
+//            Method method = configFieldClass.getMethod("set", Object.class);
+//            LOGGER.info(method);
+//            method.invoke(configField.get(ConfigHolder.CLIENT), field.get(buffer));
+//        }
+    
+        ConfigHolder.CLIENT.serverUrl.set(buffer.serverUrl);
+        ConfigHolder.CLIENT.vibrate.set(buffer.vibrate);
+        ConfigHolder.CLIENT.mode.set(buffer.mode);
+        ConfigHolder.CLIENT.stealth.set(buffer.stealth);
+        ConfigHolder.CLIENT.tickFrequency.set(ClientConfig.TickFrequencyOptions.fromInt(buffer.tickFrequency));
+                
+        ConfigHolder.CLIENT.attackIntensity.set(buffer.attackIntensity);
+        ConfigHolder.CLIENT.hurtIntensity.set(buffer.hurtIntensity);
+        ConfigHolder.CLIENT.mineIntensity.set(buffer.mineIntensity);
+        ConfigHolder.CLIENT.placeIntensity.set(buffer.placeIntensity);
+        ConfigHolder.CLIENT.xpChangeIntensity.set(buffer.xpChangeIntensity);
+        ConfigHolder.CLIENT.fishingIntensity.set(buffer.fishingIntensity);
+        ConfigHolder.CLIENT.harvestIntensity.set(buffer.harvestIntensity);
+        ConfigHolder.CLIENT.vitalityIntensity.set(buffer.vitalityIntensity);
+        ConfigHolder.CLIENT.advancementIntensity.set(buffer.advancementIntensity);
+        } catch (Throwable e)
+        {
+            LOGGER.info(e);
+        }
+    }
+
+    public static ConfigScreenHandler.ConfigScreenFactory createConfigScreenFactory() {
+        return new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new ConfigScreen(screen));
     }
 
 }
