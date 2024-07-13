1cb5a3d68607eb6be910310aa7cabb6cc8be2a3f
Sour3 <turtlewong007@gmail.com>
2024-07-13 00:36:19 -0700

    Fixed Multiplayer Issues for 1.18.2

---
+++ forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/mixin/ClientAdvancementsMixin.java
@@ -0,0 +1,38 @@
+package com.therainbowville.minegasm.mixin;
+import com.therainbowville.minegasm.client.ClientEventHandler;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientAdvancements;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AdvancementEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(ClientAdvancements.class)
+public class ClientAdvancementsMixin {
+    private static Logger LOGGER = LogManager.getLogger();
+    private static final Minecraft minecraft = Minecraft.getInstance();
+
+    @Inject(method = "update", at = @At("HEAD"), cancellable = true)
+    public void onUpdate(ClientboundUpdateAdvancementsPacket advancementInfoPacket, CallbackInfo ci) {
+        LOGGER.info("Advancement updated");
+
+        for(Map.Entry<ResourceLocation, AdvancementProgress> entry : advancementInfoPacket.getProgress().entrySet()) {
+            Advancement advancement = ((ClientAdvancements) (Object) this).getAdvancements().get(entry.getKey());
+            Player player = minecraft.player;
+            AdvancementEvent event = new AdvancementEvent(player, advancement);
+            ClientEventHandler.onAdvancementEvent(event);
+        }
+    }
+}
