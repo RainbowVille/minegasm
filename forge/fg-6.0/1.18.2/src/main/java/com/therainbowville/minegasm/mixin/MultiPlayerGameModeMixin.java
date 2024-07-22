57a56b310d2be0ddc0ada317338ab51cc9479315
Sour3 <turtlewong007@gmail.com>
2024-07-22 00:00:28 -0700

    FIxed Place Event Issues
    Fixed Issues where place event would incorrectly trigger

--- forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/mixin/MultiPlayerGameModeMixin.java
+++ forge/fg-6.0/1.18.2/src/main/java/com/therainbowville/minegasm/mixin/MultiPlayerGameModeMixin.java
@@ -1,25 +1,21 @@
 package com.therainbowville.minegasm.mixin;
 import com.therainbowville.minegasm.client.ClientEventHandler;
 
-import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
-import net.minecraftforge.event.world.BlockEvent.EntityPlaceEvent;
 
-import net.minecraft.world.level.block.Block;
-
+import net.minecraft.core.BlockPos;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.InteractionResult;
-
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,9 +28,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
     private static Logger LOGGER = LogManager.getLogger();
+    
+    boolean placedBlock;
 
     @Inject(method = "destroyBlock", at = @At("HEAD"), cancellable = true)
     public void onDestroyBlock(BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+        if (Minecraft.getInstance().isLocalServer()) { return; }
+        
         Player player = Minecraft.getInstance().player;
         if (player != null) {
             BreakEvent event = new BreakEvent(player.level, blockPos, player.level.getBlockState(blockPos), player);
@@ -42,14 +42,22 @@ public class MultiPlayerGameModeMixin {
         }
     }
 
-    @Inject(method = "useItemOn", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "useItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;", shift = At.Shift.AFTER), cancellable = true)
     public void onUseItemOn(LocalPlayer player, ClientLevel world, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir) {
-        // check if item is a block
-        ItemStack itemStack = player.getItemInHand(hand);
-        Item item = itemStack.getItem();
-        if (item instanceof BlockItem) {
+        if (Minecraft.getInstance().isLocalServer() ) { return; }
+        
+        this.placedBlock = true;
+        
+    }
+    
+    @Inject(method = "useItemOn", at = @At("RETURN"), cancellable = true)
+    public void onUseItemOnReturn(LocalPlayer player, ClientLevel world, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir) {
+        if (Minecraft.getInstance().isLocalServer() || !this.placedBlock) { return; }
+        
+        if (player.getItemInHand(hand).getItem() instanceof BlockItem && cir.getReturnValue() == InteractionResult.SUCCESS) {
             ClientEventHandler.onPlace();
         }
+        this.placedBlock = false;
     }
 
 }
